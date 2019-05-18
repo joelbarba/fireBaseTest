8aa3b0905554789e8382daade777268a2b1d584a
Joel Barba <joel.barba.vidal@gmail.com>
2019-05-18 21:52:18 +0100

    Completed CRUD using 2 forms (add / edit) and a list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import * as RxOp from 'rxjs/operators';
+
+export interface User {
+  id?: string;
+  username: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test1';
+  private userDoc: AngularFirestoreDocument<User>;
+
+  public usersList$: Observable<any[]>;
+  public newUser: User;
+  public editUser: User;
+
+  private usersCollection: AngularFirestoreCollection<User>;
+
+  constructor(private afs: AngularFirestore) {
+    this.newUser = this.getEmptyUser();
+
+    this.usersCollection = afs.collection('users');
+    // this.usersList$ = this.usersCollection.valueChanges();
+    this.usersList$ = this.usersCollection.snapshotChanges().pipe(
+      RxOp.map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as User;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+  }
+
+  public getEmptyUser = (): User => {
+    return {
+      username: null,
+      email: null,
+      first_name: null,
+      last_name: null,
+    };
+  }
+
+  public addNewUser = () => {
+    this.usersCollection.add(this.newUser);
+    this.newUser = this.getEmptyUser();
+  }
+
+  public deleteUser = (user: User) => {
+    this.userDoc = this.afs.doc<User>('users/' + user.id);
+    this.userDoc.delete();
+
+    // If the user is loaded in the form, remove it
+    if (user.id === this.editUser.id) { this.editUser = null; }
+  }
+
+  public loadUser = (user: User) => {
+    this.userDoc = this.afs.doc<User>('users/' + user.id);
+
+    // public item: Observable<User>;
+    // this.user$ = this.userDoc.valueChanges();
+    const subs = this.userDoc.snapshotChanges().subscribe(state => {
+      this.editUser = state.payload.data();
+      this.editUser.id = state.payload.id;
+      subs.unsubscribe(); // Just take the value once
+    });
+  }
+
+  public saveUser = () => {
+    this.userDoc.update(this.editUser);
+  }
+
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,18 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// firebaseConfig : {
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyChchHYYxoL1Z6sjbTWvI3G3WgwbhHJdx8',
+    authDomain: 'test1-8e3fd.firebaseapp.com',
+    databaseURL: 'https://test1-8e3fd.firebaseio.com',
+    projectId: 'test1-8e3fd',
+    storageBucket: 'test1-8e3fd.appspot.com',
+    messagingSenderId: '5248003648',
+    appId: '1:5248003648:web:a7d2eef4824cd3c9'
+  }
 };
 
 /*
